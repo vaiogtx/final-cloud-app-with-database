6aee544e0e8a1ebda7906d33452e9b0d0b224748
Angus <131868779+vaiogtx@users.noreply.github.com>
2023-05-26 14:07:14 +0700

    [add] models
    Question, Choice, Submission model added.
    Registered to admin.py.
    Superuser created.

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
+
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
@@ -8,6 +9,16 @@ from .models import Course, Lesson, Instructor, Learner
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
+
+
+class QuestionInLine(admin.StackedInline):
+    model = Question
+    extra = 5
+
+
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 4
 
 
 # Register your models here.
@@ -19,12 +30,19 @@ class CourseAdmin(admin.ModelAdmin):
 
 
 class LessonAdmin(admin.ModelAdmin):
+    inlines = [QuestionInLine]
     list_display = ['title']
 
 
-# <HINT> Register Question and Choice models here
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInLine]
 
+
+# <HINT> Register Question and Choice models here
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
